75419bf0abdbb0877641119780f5e3c304f0c327
Adel Hassan <sean.dashals@gmail.com>
2023-08-09 19:16:55 -0500

    Fixed bug with only one section

--- para_encoder.py
+++ para_encoder.py
@@ -4,10 +4,12 @@ from torch import Tensor
 from transformers import AutoTokenizer, AutoModel
 import numpy as np
 import pandas as pd
+import logging
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import invisibility_of_element_located
 from selenium.common.exceptions import TimeoutException
 from csv import DictReader, DictWriter
 import re
@@ -41,34 +43,65 @@ model_thread.start()
 pinecone.init(api_key=os.getenv('PINEKEY'), environment="us-west4-gcp-free")
 index = pinecone.Index("utd-llm-para")
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome()
+def init_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument("window-size=1400,1500")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("start-maximized")
+    options.add_argument("enable-automation")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options)
 
-# shoot, i'm gonna have to log in if i actually want to read the articles
-driver.get('https://uptodate.com/login')
-un_inp = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, value="userName"))
-un_inp.send_keys(os.getenv('UN'))
-pw_inp = driver.find_element(By.ID, value="password")
-pw_inp.send_keys(os.getenv('PW'))
-pw_inp = driver.find_element(By.ID, value="password")
-login_btn = driver.find_element(By.CLASS_NAME, value="login-button")
-login_btn.click()
-driver.implicitly_wait(5)
-print('Logged in')
+    # shoot, i'm gonna have to log in if i actually want to read the articles
+    driver.get('https://uptodate.com/login')
+    un_inp = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, value="userName"))
+    un_inp.send_keys(os.getenv('UN'))
+    pw_inp = driver.find_element(By.ID, value="password")
+    pw_inp.send_keys(os.getenv('PW'))
+    pw_inp = driver.find_element(By.ID, value="password")
+    login_btn = driver.find_element(By.CLASS_NAME, value="login-button")
+    login_btn.click()
+    driver.implicitly_wait(5)
+    print('Logged in')
+    return driver
+driver = init_driver()
+
 try:
-    articles = pd.read_csv('final_articles.csv').sample(frac=1, random_state=4242)
+    articles = pd.read_csv('final_articles.csv').drop_duplicates('href').sample(frac=1, random_state=4242)
+    article_num = -1
     for row in articles.itertuples():
+        # to help with skipping later
+        article_num += 1
+        print('Fetching article', article_num)
+        if article_num < 332:
+            continue
+        url_stub = row.href[len('https://www.uptodate.com/contents/'):]
+        if url_stub.startswith('society-guideline-links') or url_stub.startswith('table-of-contents'):
+            print('Skipping', row.href)
+            continue
         driver.get(row.href)
-            
-        topic_text = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, value="topicText"))
-        driver.implicitly_wait(3)
-        # driver.execute_script('document.querySelector("#topicText").innerHTML = '
-        #                       'document.querySelector("#topicText").innerHTML.replaceAll('
-        #                       '/<p class="headingAnchor" id="xhash_H.+?<\/p>/g, "<br />");')
+        
+        try:
+            topic_text = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, value="topicText"))
+            WebDriverWait(driver, timeout=5).until(invisibility_of_element_located((By.CSS_SELECTOR, 'a[href="/store"]')))
+        except TimeoutException:
+            print(f"Timed out on {article_num}")
+            # probably some sort of rate-limiting, so eventually, we'll probably sleep it off
+            # but for now, we'll try printing the HTML once
+            # print(driver.find_element(By.TAG_NAME, "body").get_attribute("innerText"))
+            # looked like the search page for some reason. Imma try turning it off and on
+            driver.quit()
+            driver = init_driver()
+            driver.get(row.href)
+            topic_text = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.ID, value="topicText"))
+            WebDriverWait(driver, timeout=5).until(invisibility_of_element_located((By.CSS_SELECTOR, 'a[href="/store"]')))
         topic_html = topic_text.get_attribute('innerText')
         paras = re.split(r'\n+', topic_html)
-        print(f"Found {len(paras)} sectons within {row.title}")
+        print(f"Found {len(paras)} paragraphs within {row.title}")
     
         para_count = 0
         curr_header = None
@@ -76,12 +109,13 @@ try:
         texts = []
         curr_text = 'passage: '
         for para in paras:
-            # if curr_header is None, then this is just INTRODUCTION and nothing's been said yet
-            if para.isupper() and curr_header is not None:
-                titles.append(curr_header)
-                texts.append(curr_text)
-                curr_header = row.href[len('https://www.uptodate.com/contents/'):] + '/#' + para
-                curr_header = curr_header
+            if para.isupper():
+                # if curr_header is None, then this is just INTRODUCTION and nothing's been said yet
+                if curr_header is not None:
+                    titles.append(curr_header)
+                    texts.append(curr_text)
+                # but we still need to reset and prepare the next section
+                curr_header = row.href[len('https://www.uptodate.com/contents/'):] + '/#' + para.strip().encode('ascii', errors='xmlcharrefreplace').decode()
                 curr_text = 'passage: '
             elif len(para) > 3:
                 curr_text += para + '\n'
@@ -94,8 +128,6 @@ try:
         # Tokenize the input texts
         if model is None:
             model_thread.join()
-        else:
-            print(tokenizer)
         batch_dict = tokenizer(texts, max_length=512, padding=True, truncation=True, return_tensors='pt')
         print("Tokenized batch_dict")
 
@@ -107,9 +139,8 @@ try:
             embeddings = average_pool(outputs.last_hidden_state,attn_mask)
             index.upsert(list(zip(titles, embeddings.tolist())))
             print('Updated index')
-        break
 except Exception as e:
-    print(e)
+    logging.exception(e)
 finally:
-    # driver.quit()
-    print(index)
+    driver.quit()
+    print('Done')
